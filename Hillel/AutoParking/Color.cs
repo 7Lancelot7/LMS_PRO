da4c37058d797e4f89c2501132c127a623f66b86
Filipp Kostetskyi <filipp.kostetskyi@student.tuke.sk>
2023-10-12 19:29:01 +0200

    added validator for Color.cs

--- Hillel/AutoParking/Color.cs
+++ Hillel/AutoParking/Color.cs
@@ -6,9 +6,59 @@ namespace Hillel;
 public struct Color
 {
     private byte _red;
+    
     private byte _green;
+    
     private byte _blue;
+    
     private byte _opacity;
+
+    /// <summary>
+    /// Checks if the red component is within a valid range (0-255).
+    /// </summary>
+    /// <exception cref="Exception"></exception>
+    private void IsRedValidValue()
+    {
+        if (_red < 0 || _red > 255) throw new Exception("No valid value for Red. Enter value 0-255");
+    }
+
+    /// <summary>
+    /// Checks if the green component is within a valid range (0-255).
+    /// </summary>
+    /// <exception cref="Exception"></exception>
+    private void IsBlueValidValue()
+    {
+        if (_green < 0 || _green > 255) throw new Exception("No valid value for Green. Enter value 0-255");
+    }
+
+    /// <summary>
+    /// Checks if the blue component is within a valid range (0-255).
+    /// </summary>
+    /// <exception cref="Exception"></exception>
+    private void IsGreenValidValue()
+    {
+        if (_blue < 0 || _blue > 255) throw new Exception("No valid value for Blue. Enter value 0-255");
+    }
+
+    /// <summary>
+    /// Checks if the opacity component is within a valid range (0-100).
+    /// </summary>
+    /// <exception cref="Exception"></exception>
+    private void IsOpacityValidValue()
+    {
+        if (_opacity < 0 || _opacity > 100) throw new Exception("No valid value for Opacity. Enter value 0-100");
+    }
+
+    /// <summary>
+    /// Checks if the values of red, green, blue, and opacity are within valid ranges.
+    /// </summary>
+    private void CheckValues()
+    {
+        IsGreenValidValue();
+        IsRedValidValue();
+        IsBlueValidValue();
+        IsOpacityValidValue();
+    }
 
     /// <summary>
     /// Gets or sets the red component of the color. The value should be between 0 and 255.
@@ -17,7 +67,11 @@ public struct Color
     {
         get => _red;
 
-        set => _red = value;
+        set
+        {
+            _red = value;
+            IsRedValidValue();
+        }
     }
 
     /// <summary>
@@ -27,7 +81,11 @@ public struct Color
     {
         get => _green;
 
-        set => _green = value;
+        set
+        {
+            _green = value;
+            IsGreenValidValue();
+        }
     }
 
     /// <summary>
@@ -37,7 +95,11 @@ public struct Color
     {
         get => _blue;
 
-        set => _blue = value;
+        set
+        {
+            _blue = value;
+            IsBlueValidValue();
+        }
     }
 
     /// <summary>
@@ -47,7 +109,11 @@ public struct Color
     {
         get => _opacity;
 
-        set => _opacity = value;
+        set
+        {
+            _opacity = value;
+            IsOpacityValidValue();
+        }
     }
 
     /// <summary>
@@ -59,6 +125,7 @@ public struct Color
         _green = 0;
         _blue = 0;
         _opacity = 0;
+        CheckValues();
     }
 
     /// <summary>
@@ -74,14 +141,16 @@ public struct Color
         _green = green;
         _blue = blue;
         _opacity = opacity;
+        CheckValues();
     }
 
     /// <summary>
     /// Initializes a new instance of the Color struct with a specified opacity value and default red, green, and blue components.
     /// </summary>
     /// <param name="opacity">The opacity component of the color. The value should be between 0 and 255.</param>
-    public Color(byte opacity):this()
+    public Color(byte opacity) : this()
     {
         _opacity = opacity;
+        CheckValues();
     }
 }

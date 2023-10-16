15cbef96906b8c5218ea91b426bb6d67af3d05f8
Filipp Kostetskyi <filipp.kostetskyi@student.tuke.sk>
2023-10-16 18:59:51 +0200

    refactor DistanceBetweenPoints

--- Hillel/AutoParking/Program.cs
+++ Hillel/AutoParking/Program.cs
@@ -6,18 +6,21 @@
         {
             Random random = new Random();
             Color color1 = new Color( 
-                (byte)random.Next(0, 256));
+                (byte)random.Next(0, 101));
             Color color2 = new Color((byte)random.Next(0, 256), (byte)random.Next(0, 256), (byte)random.Next(0, 256),
-                (byte)random.Next(0, 256));
+                (byte)random.Next(0, 101));
             Color color3 = new Color((byte)random.Next(0, 256), (byte)random.Next(0, 256), (byte)random.Next(0, 256),
-                (byte)random.Next(0, 256));
+                (byte)random.Next(0, 101));
 
             Car lexus = new Car("Lexus", "LS", 7777, color1);
             Car bmv = new Car("BMW", "528", 1111, color2);
             Car mercedes = new Car("Mercedes", "E53", 1234, color3);
 
-            #nullable enable
-            string hello = null;
+            List<int> a = new List<int>();
+            a.Add(1);
+            a.Add(2);
+            a.Add(3);
+            
 
             using Parking parking = new Parking(5, "Nikolskyi", "Kharkiv,Ukraine");
             parking.Park(lexus);

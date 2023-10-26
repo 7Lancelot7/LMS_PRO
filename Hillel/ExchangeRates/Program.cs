e062a53577bc97f660cc3019a935a9eaf3c04467
Filipp Kostetskyi <filipp.kostetskyi@student.tuke.sk>
2023-10-26 17:57:52 +0200

    update proj

---
+++ Hillel/ExchangeRates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace ExchangeRates
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            ExchangeRates<double> exchangeRates = new ExchangeRates<double>();
+            exchangeRates.ChangeRate(Currency.USD,61.1);
+            exchangeRates.ChangeRate(Currency.USD,62.4);
+            exchangeRates.ChangeRate(Currency.USD,63.6);
+            exchangeRates.ChangeRate(Currency.EUR,70.345);
+            exchangeRates.ChangeRate(Currency.EUR,71.421);
+            Console.WriteLine();
+            Console.WriteLine();
+            exchangeRates.PrintHistory();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Hillel/VendingCoffeeMachine/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using VendingCoffeeMachine;
+using VendingCoffeeMachine.Contracts;
+
+class Program
+{
+    public static void AddCoffee(ICoffee coffee)
+    {
+        coffee.AddCoffee();
+    } 
+    public static void AddSugar(ISugar coffee)
+    {
+        coffee.AddSugar();
+    } 
+    public static void AddWater(IWater coffee)
+    {
+        coffee.AddWater();
+    } 
+    public static void AddMilk(IMilk coffee)
+    {
+        coffee.AddMilk();
+    } 
+    
+    public static void Main()
+    {
+        Cup cup = new Cup();
+        //cup.Stir();
+        AddCoffee(cup);
+        AddWater(cup);
+        AddMilk(cup);
+        AddSugar(cup);
+        cup.Stir();
+
+    }
+}
